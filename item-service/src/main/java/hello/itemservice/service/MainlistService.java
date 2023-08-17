6ff9db5be73f26772aa7a3933a2a3d8c211002b9
이상범 <83106958+lsangbum@users.noreply.github.com>
2023-08-18 00:49:15 +0900

    마이바티스추가

--- item-service/src/main/java/hello/itemservice/service/MainlistService.java
+++ item-service/src/main/java/hello/itemservice/service/MainlistService.java
@@ -1,9 +1,12 @@
 package hello.itemservice.service;
 
+import hello.itemservice.mapper.MainlistMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainlistService {
+
+    private MainlistMapper mainlistMapper;
 
 
 }
