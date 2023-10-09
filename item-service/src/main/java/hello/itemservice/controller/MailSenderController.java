cd97afe3d5eaaadacd8627ef6fc6b12a2f91baf0
이상범 <83106958+lsangbum@users.noreply.github.com>
2023-10-09 23:24:30 +0900

    231009
    session 대신 JWT를 써봐야겠다

--- item-service/src/main/java/hello/itemservice/controller/MailSenderController.java
+++ item-service/src/main/java/hello/itemservice/controller/MailSenderController.java
@@ -8,16 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 @RestController
 @AllArgsConstructor
 @Slf4j
